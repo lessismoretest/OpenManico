7eaa89668b6f3a7e1641376281d33149917f85c6
Pokkepokke <53515721+lessismoretest@users.noreply.github.com>
2025-01-15 01:26:40 +0800

    Initial Commit

---
+++ OpenManico/OpenManicoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenManicoApp.swift
+//  OpenManico
+//
+//  Created by Less is more on 2025/1/15.
+//
+
+import SwiftUI
+
+@main
+struct OpenManicoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OpenManicoTests/OpenManicoTests.swift
@@ -0,0 +1,17 @@
+//
+//  OpenManicoTests.swift
+//  OpenManicoTests
+//
+//  Created by Less is more on 2025/1/15.
+//
+
+import Testing
+@testable import OpenManico
+
+struct OpenManicoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
